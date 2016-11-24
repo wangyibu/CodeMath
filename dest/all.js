983ca67e300e7158ae9fb1d99514ac2890643bb5
王一卜 <wangyibu@gitlab.gridsum.com>
2016-11-24 22:30:16 +0800

    11

--- dest/all.js
+++ dest/all.js
@@ -1,3 +1,8 @@
+var app = {};
+
+app.firstMethod = function(){
+    alert('This is from first method');
+}
 app.secondMethod = function(){
     console.log(123);
     alert('This is from second method');
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var copy = require('gulp-contrib-copy');
+var concat = require('gulp-concat');
 
 gulp.task('copydir1', function() {
     gulp.src('dir1/**')
@@ -7,6 +8,13 @@ gulp.task('copydir1', function() {
         .pipe(gulp.dest('dir2/'));
 });
 
-// gulp.watch('files', ['tasks']);
-// gulp.src('files')
-//   .pipe(gulp.dest('build'))
+gulp.task('t1', function() {
+    return gulp.src('src/**/*.js')
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('dest'));
+});
+
+
+gulp.task('w1', function() {
+    gulp.watch('src/**/*.js', ['t1']);
+});
